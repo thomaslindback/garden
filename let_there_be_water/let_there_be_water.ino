a9f2b3e859c7d4fd6c5186269d5db51fbd50dd25
Thomas Lindbäck <thomas.lindback@gmail.com>
2018-05-31 00:25:58 +0200

    First test

--- let_there_be_water/let_there_be_water.ino
+++ let_there_be_water/let_there_be_water.ino
@@ -1,11 +1,22 @@
+#ifndef __AVR_ATtiny85__
+  #define __AVR_ATtiny85__
+#endif
 
+#include <Arduino.h>
 
+int led = 1; // blink 'digital' pin 1 - AKA the built in red LED
+ 
+// the setup routine runs once when you press reset:
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+ 
 }
-
+ 
+// the loop routine runs over and over again forever:
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    digitalWrite(led, HIGH); 
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(500);
 }
